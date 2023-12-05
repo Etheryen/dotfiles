13341f075c26aa99942151df4dd21b120fa3c9f3
Etheryen <lukasz.ciwoniuk@gmail.com>
2023-12-05 21:23:43 +0100

    add tmux and .gitignore

--- .config/nvim/lua/colorscheme.lua
+++ .config/nvim/lua/colorscheme.lua
@@ -50,6 +50,23 @@ local colorschemes = {
 			vim.cmd.colorscheme("gruvbox")
 		end,
 	},
+	["onedark"] = {
+		"navarasu/onedark.nvim",
+		name = "onedark",
+		priority = 1000,
+		config = function()
+			require("onedark").setup({
+				code_style = {
+					comments = "none",
+					keywords = "none",
+					functions = "none",
+					strings = "none",
+					variables = "none",
+				},
+			})
+			vim.cmd.colorscheme("onedark")
+		end,
+	},
 }
 
 return colorschemes[GLOBAL_COLORSCHEME]
