69e5c9f291ad4611f67f7e3b19ecbebff550f4cc
Etheryen <lukasz.ciwoniuk@gmail.com>
2024-09-16 23:42:50 +0200

    feat(lint): use dotfiles .eslint config if none in project

--- .config/nvim/after/plugin/linting.lua
+++ .config/nvim/after/plugin/linting.lua
@@ -9,18 +9,57 @@ lint.linters_by_ft = {
 	["*"] = { "codespell" },
 }
 
-local lint_autogroup = vim.api.nvim_create_augroup("lint", { clear = true })
+lint.linters.eslint_d.args = (function()
+	local function config_exists()
+		local names = {
+			".eslintrc.json",
+			".eslintrc.js",
+			".eslintrc.cjs",
+			".eslintrc.mjs",
+			".eslintrc.yaml",
+			".eslintrc.yml",
+			".eslintrc",
+			"eslint.config.js",
+			"eslint.config.mjs",
+			"eslint.config.cjs",
+			"eslint.config.ts",
+			"eslint.config.mts",
+			"eslint.config.cts",
+		}
+		for _, v in ipairs(names) do
+			if vim.fn.filereadable(v) == 1 then
+				return true
+			end
+		end
+		return false
+	end
 
-lint.linters.eslint_d.args = {
-	"--no-config-lookup", -- <-- this is the key argument
-	"--format",
-	"json",
-	"--stdin",
-	"--stdin-filename",
-	function()
-		return vim.api.nvim_buf_get_name(0)
-	end,
-}
+	if config_exists() then
+		return {
+			"--format",
+			"json",
+			"--stdin",
+			"--stdin-filename",
+			function()
+				return vim.api.nvim_buf_get_name(0)
+			end,
+		}
+	end
+
+	return {
+		"--config",
+		"/home/etheryen/.eslint/eslint.config.mjs",
+		"--format",
+		"json",
+		"--stdin",
+		"--stdin-filename",
+		function()
+			return vim.api.nvim_buf_get_name(0)
+		end,
+	}
+end)()
+
+local lint_autogroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
 vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
 	group = lint_autogroup,
@@ -29,6 +68,6 @@ vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
 	end,
 })
 
-vim.keymap.set("n", "<leader>l", function()
+vim.keymap.set("n", "<leader>ll", function()
 	lint.try_lint()
 end, { desc = "Trigger linting for current file" })
