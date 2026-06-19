3c62a35b5bcc3e58c533642102704801c30658f8
Etheryen <lukasz.ciwoniuk@gmail.com>
2026-06-19 22:52:10 +0200

    feat: themes, xset settings, picom

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -8,3 +8,26 @@ return {
 		vim.cmd.colorscheme("catppuccin-nvim")
 	end,
 }
+
+-- return { "ellisonleao/gruvbox.nvim", priority = 1000, config = true, opts = ... }
+-- return {
+-- 	"ellisonleao/gruvbox.nvim",
+-- 	lazy = false,
+-- 	priority = 1000,
+-- 	config = true,
+-- 	opts = {},
+-- 	init = function()
+-- 		vim.cmd.colorscheme("gruvbox")
+-- 	end,
+-- }
+--
+-- return {
+-- 	"blazkowolf/gruber-darker.nvim",
+-- 	lazy = false,
+-- 	priority = 1000,
+-- 	config = true,
+-- 	opts = {},
+-- 	init = function()
+-- 		vim.cmd.colorscheme("gruber-darker")
+-- 	end,
+-- }
